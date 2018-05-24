65a23d30f93daa849eb5ad84d473a268339978d0
Ian Bryan <incomplete@users.noreply.github.com>
2018-05-24 09:16:44 -0700

    Initial Commit
    Added description to the List class. Will need follow-up description for Node inner class.

---
+++ src/List.java
@@ -0,0 +1,23 @@
+/* *
+*<h4>Description:</h4> 
+*<br>
+*<p>
+* The List class here is a simple LinkedList structure that will be used as the
+* super class to a Stack and Queue class. The methods here will allow for
+* inserting and removing of nodes at certain indices in the list. 
+* 
+* This class contains an inner class called Node which is used to store the data from
+* the Object that is being manipulated in the list.
+*</p>
+*
+*@author Ian Bryan
+* */
+public class List {
+	
+	/*
+	 * A little pseudocode to go with the class
+	 * 
+	 * 
+	 * */
+
+}
